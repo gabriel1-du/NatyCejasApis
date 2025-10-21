f66cdf3a35b9cc831a3e7f8c14beccf644708170
Gabriel Duran <gabrieldurans999@gmail.com>
2025-10-21 12:29:34 -0300

    Se crea archivos como intermediario entre esta api y la api de inventario para restar stock(check)

--- InventarioApi/src/main/java/com/example/InventarioApi/Controller/ProductoControllerFolder/ProductoController.java
+++ InventarioApi/src/main/java/com/example/InventarioApi/Controller/ProductoControllerFolder/ProductoController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import java.nio.file.Files;
 import java.nio.file.Path;
