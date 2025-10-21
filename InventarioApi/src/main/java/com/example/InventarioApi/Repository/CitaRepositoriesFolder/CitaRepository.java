f58f1977e9ff40084296938ee91e1d18fbbc50b2
Bryan JImenez <bry.jimenez@duocuc.cl>
2025-10-21 19:20:15 -0300

    Revert "Se añade metodos crud para una nueva entidad en la base de datos la cual es Cita"
    This reverts commit 99c22b9202733afdc2c09b3de34a919737e87146.

--- InventarioApi/src/main/java/com/example/InventarioApi/Repository/CitaRepositoriesFolder/CitaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.InventarioApi.Repository.CitaRepositoriesFolder;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.InventarioApi.Model.Cita;
-
-public interface CitaRepository extends JpaRepository<Cita, Integer> {
-}
